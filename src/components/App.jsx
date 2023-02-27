ca29a18a9a1da0382efcb08a006d47b45e4673c7
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-02-27 11:15:52 +0600

    base folder added

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Navbar/>
+    </>
+  );
+};
+
+export default App;
