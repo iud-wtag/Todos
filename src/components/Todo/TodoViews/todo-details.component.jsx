d36f8c91cc4ec79f6df337671c3dd27014cedc79
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-20 10:48:28 +0600

    updated: naming convention for data

--- src/components/Todo/TodoViews/todo-details.component.jsx
+++ src/components/Todo/TodoViews/todo-details.component.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const TodoDetails = ({ list }) => {
   return (
     <div className="todo-view__details">
-      <h3 className="todo-view__title">{list.data}</h3>
+      <h3 className="todo-view__title">{list.task}</h3>
       <p className="todo-view__create-time">Created At: {list.date}</p>
     </div>
   );
--- src/components/Todo/todo.jsx
+++ src/components/Todo/todo.jsx
@@ -28,7 +28,7 @@ const Todo = () => {
     setInputData(e.target.value);
     toggleEmptyError(false);
   };
-  const handleAddTask = (e) => {
+  const handleAddTask = () => {
     const sanitizedData = sanitizeInput(inputData);
     if (sanitizedData.trim() === "") {
       toggleEmptyError(true);
