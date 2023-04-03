473e37b64171d63cbb0458e7977f0c1108f0e594
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-03 12:59:17 +0600

    added: filtering css

--- src/components/Todo/Topbar/filter-todo.component.jsx
+++ src/components/Todo/Topbar/filter-todo.component.jsx
@@ -3,10 +3,16 @@ import PropTypes from "prop-types";
 import { ALL, INCOMPLETE, COMPLETE } from "common/constants";
 
 const FilterTodo = ({ handleFilterClick }) => {
+  document.querySelectorAll(".filter-btn__btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelector(".active").classList.remove("active");
+      btn.classList.add("active");
+    });
+  });
   return (
     <div className="filter-btn">
       <button
-        className="filter-btn__btn btn white-back__btn"
+        className="filter-btn__btn btn white-back__btn active"
         onClick={() => handleFilterClick(ALL)}
       >
         All
