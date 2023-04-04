82fd0e58030b66e60d6aabc01d4e4e32bf1d8243
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-04 12:12:46 +0600

    added: search input

--- src/components/Todo/Navbar/search-bar.component.jsx
+++ src/components/Todo/Navbar/search-bar.component.jsx
@@ -3,10 +3,20 @@ import searchIcon from "assets/images/search.png";
 import { SEARCH_ICON } from "common/constants";
 
 const SearchBar = () => {
+  const toggleSearchInput = () => {
+    document.querySelector("#todo-search_input").classList.toggle("active");
+  };
+  const handleSearchChange = () => {};
   return (
-    <>
-      <img src={searchIcon} alt={SEARCH_ICON} />
-    </>
+    <div className="todo-search">
+      <input
+        type="text"
+        id="todo-search_input"
+        className="todo_search_input"
+        onChange={handleSearchChange}
+      />
+      <img src={searchIcon} alt={SEARCH_ICON} onClick={toggleSearchInput} />
+    </div>
   );
 };
 
