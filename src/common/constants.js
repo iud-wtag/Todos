d682e0202770d6114a0d9f6a1c782669279db3f2
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-05-02 14:25:24 +0600

    alt tag constants name updated

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,7 @@
 export const CREATE_ICON = "Create Icon";
 export const SEARCH_ICON = "Search Icon";
-export const ALT_COMPLETE_BUTTON = "Complete Button";
-export const ALT_EDIT_BUTTON = "Edit Button";
-export const ALT_DELETE_BUTTON = "Delete Button";
+export const ALT_TEXT_COMPLETE_TODO = "Complete Button";
+export const ALT_TEXT_EDIT_TODO = "Edit Button";
+export const ALT_TEXT_DELETE_TODO = "Delete Button";
 export const ALT_LOGO = "Logo";
 export const KEY_ENTER = "Enter";
--- src/components/Todo/TodoCards/todo-action-bar.component.jsx
+++ src/components/Todo/TodoCards/todo-action-bar.component.jsx
@@ -3,22 +3,22 @@ import checkButton from "assets/images/check.png";
 import editButton from "assets/images/edit.png";
 import deleteButton from "assets/images/delete.png";
 import {
-  ALT_COMPLETE_BUTTON,
-  ALT_EDIT_BUTTON,
-  ALT_DELETE_BUTTON,
+  ALT_TEXT_COMPLETE_TODO,
+  ALT_TEXT_EDIT_TODO,
+  ALT_TEXT_DELETE_TODO,
 } from "common/constants";
 
 const TodoActionBar = () => {
   return (
     <div className="todo__card-view-btns">
       <button>
-        <img src={checkButton} alt={ALT_COMPLETE_BUTTON} />
+        <img src={checkButton} alt={ALT_TEXT_COMPLETE_TODO} />
       </button>
       <button>
-        <img src={editButton} alt={ALT_EDIT_BUTTON} />
+        <img src={editButton} alt={ALT_TEXT_EDIT_TODO} />
       </button>
       <button>
-        <img src={deleteButton} alt={ALT_DELETE_BUTTON} />
+        <img src={deleteButton} alt={ALT_TEXT_DELETE_TODO} />
       </button>
     </div>
   );
