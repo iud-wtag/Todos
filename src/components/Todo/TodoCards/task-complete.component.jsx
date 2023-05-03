bc8ed613cbddb873a2061f67f76312b3bf421774
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-05-03 10:34:53 +0600

    constant updated for icons

--- src/components/Todo/TodoCards/task-complete.component.jsx
+++ src/components/Todo/TodoCards/task-complete.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import deleteIcon from "assets/images/delete.png";
-import { ALT_TEXT_DELETE_TODO } from "common/constants";
+import { ICON_DELETE, ALT_TEXT_DELETE_TODO } from "common/constants";
 
 const TaskComplete = ({ todo, onDeleteTask }) => {
   const { id, completeTime } = todo;
@@ -13,7 +12,7 @@ const TaskComplete = ({ todo, onDeleteTask }) => {
   return (
     <div className="todo__card-view-btns complete-btns">
       <button onClick={handleDeleteTask}>
-        <img src={deleteIcon} alt={ALT_TEXT_DELETE_TODO} />
+        <img src={ICON_DELETE} alt={ALT_TEXT_DELETE_TODO} />
       </button>
       <p className="complete-msg">
         Completed in {completeTime} {completeTime <= 1 ? "day" : "days"}
--- src/components/Todo/TodoCards/task-incomplete.component.jsx
+++ src/components/Todo/TodoCards/task-incomplete.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import checkIcon from "assets/images/check.png";
-import editIcon from "assets/images/edit.png";
-import deleteIcon from "assets/images/delete.png";
 import {
+  ICON_CHECK,
+  ICON_EDIT,
+  ICON_DELETE,
   ALT_TEXT_COMPLETE_TODO,
   ALT_TEXT_EDIT_TODO,
   ALT_TEXT_DELETE_TODO,
@@ -23,13 +23,13 @@ const TaskIncomplete = ({ todo, onDeleteTask, onCompleteTask }) => {
   return (
     <div className="todo__card-view-btns incomplete-btns">
       <button onClick={handleCompleteTask}>
-        <img src={checkIcon} alt={ALT_TEXT_COMPLETE_TODO} />
+        <img src={ICON_CHECK} alt={ALT_TEXT_COMPLETE_TODO} />
       </button>
       <button>
-        <img src={editIcon} alt={ALT_TEXT_EDIT_TODO} />
+        <img src={ICON_EDIT} alt={ALT_TEXT_EDIT_TODO} />
       </button>
       <button onClick={handleDeleteTask}>
-        <img src={deleteIcon} alt={ALT_TEXT_DELETE_TODO} />
+        <img src={ICON_DELETE} alt={ALT_TEXT_DELETE_TODO} />
       </button>
     </div>
   );
