271bb934c1c9e4480da0a2b8b94201fabd2fb79d
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-05-10 10:46:47 +0600

    responsive css added and filter functionality added for mobile select

--- src/components/Todos/Topbar/filter-todo.component.jsx
+++ src/components/Todos/Topbar/filter-todo.component.jsx
@@ -24,21 +24,48 @@ const FilterTodo = ({ activeFilterType, onActiveFilterType }) => {
     onActiveFilterType(label);
   }
 
+  function handleSelectFilter(event) {
+    const label = event.target.value;
+    dispatch(handleCurrentPage(1));
+    dispatch(filterTodo(label));
+    onActiveFilterType(label);
+  }
+
   return (
-    <div className="todo__top__btn-filter">
-      {filterButtons.map((button) => (
-        <button
-          key={button.label}
-          className={`todo__top__btn-filter__inner btn__bg-white ${
-            activeFilterType === button.label ? "active" : ""
-          }`}
-          data-label={`${button.label}`}
-          onClick={handleFilter}
+    <>
+      <div className="todo__top__btn-filter">
+        {filterButtons.map((button) => (
+          <button
+            key={button.label}
+            className={`todo__top__btn-filter__inner btn__bg-white ${
+              activeFilterType === button.label ? "active" : ""
+            }`}
+            data-label={button.label}
+            onClick={handleFilter}
+          >
+            {button.label}
+          </button>
+        ))}
+      </div>
+      <div className="todo__top__btn-filter-select">
+        <select
+          name={activeFilterType}
+          value={activeFilterType}
+          onChange={handleSelectFilter}
+          className="todo__top__btn-filter__inner"
         >
-          {button.label}
-        </button>
-      ))}
-    </div>
+          {filterButtons.map((button) => (
+            <option
+              key={button.label}
+              value={button.label}
+              data-label={button.label}
+            >
+              {button.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 };
 
