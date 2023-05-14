f44db44576e470b569c369545cf67ee1f66be239
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-05-14 19:41:43 +0600

    add: toast dismiss for 1 toast at a time

--- src/common/notification.js
+++ src/common/notification.js
@@ -2,18 +2,21 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const showSuccessToast = (message) => {
+  toast.dismiss();
   toast.success(message, {
     className: "toast__message message__success",
   });
 };
 
 export const showErrorToast = (message) => {
+  toast.dismiss();
   toast.error(message, {
     className: "toast__message message__error",
   });
 };
 
 export const showRequiredToast = (message) => {
+  toast.dismiss();
   toast.warn(message, {
     className: "toast__message message__warn",
   });
