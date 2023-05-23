fbea78f3e6db00e4e99907f2310b455d74460767
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-05-23 18:25:01 +0600

    add: test packages and test for navbar

--- src/components/Todos/Navbar/__test__/navbar.test.js
+++ src/components/Todos/Navbar/__test__/navbar.test.js
@@ -1,42 +1,55 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import store from "store";
+import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import Logo from "components/Todos/Navbar/logo.component";
 import SearchBar from "components/Todos/Navbar/search-bar.component";
+import userEvent from "@testing-library/user-event";
+
+const mockStore = configureStore([]);
+
+const MockSearchBar = ({ store }) => {
+  return (
+    <Provider store={store}>
+      <SearchBar />
+    </Provider>
+  );
+};
 
 describe("<Navbar>", () => {
-  it("renders todo logo image", async () => {
+  const user = userEvent.setup();
+
+  it("renders todo logo image and text", async () => {
     render(<Logo />);
     const logoElement = screen.getByAltText("Logo");
-    expect(logoElement).toBeInTheDocument();
-  });
-
-  it("renders todo logo text", async () => {
-    render(<Logo />);
     const headingElement = screen.getByRole("heading", { name: "Todos" });
+
+    expect(logoElement).toBeInTheDocument();
     expect(headingElement).toBeInTheDocument();
   });
 
-  it("renders search icon", async () => {
-    render(
-      <Provider store={store}>
-        <SearchBar />
-      </Provider>
-    );
-    const buttonElement = screen.getByAltText(/Search Icon/i);
-    expect(buttonElement).toBeInTheDocument();
+  it("renders search icon and search input not visible", async () => {
+    let store = mockStore({
+      buttonClickReducers: { isSearchButtonClicked: false },
+    });
+    render(<MockSearchBar store={store} />);
+
+    const searchIcon = screen.getByAltText(/Search Icon/i);
+    const searchInput = screen.queryByPlaceholderText(/search here.../i);
+
+    expect(searchIcon).toBeInTheDocument();
+    expect(searchInput).not.toBeInTheDocument();
   });
 
-  it("render search input when icon clicked", async () => {
-    render(
-      <Provider store={store}>
-        <SearchBar />
-      </Provider>
-    );
-    const searchButtonElement = screen.getByAltText(/Search Icon/i);
-    fireEvent.click(searchButtonElement);
-    const searchInputElement = screen.queryByPlaceholderText("Search here...");
-    expect(searchInputElement).toBeInTheDocument();
+  it("renders search input", async () => {
+    let store = mockStore({
+      buttonClickReducers: { isSearchButtonClicked: true },
+    });
+    render(<MockSearchBar store={store} />);
+    const searchInput = screen.queryByPlaceholderText(/search here.../i);
+
+    await user.type(searchInput, "task todo");
+    expect(searchInput).toBeInTheDocument();
+    expect(searchInput).toHaveValue("task todo");
   });
 });
