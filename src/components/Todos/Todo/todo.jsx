6342ad14859309cb9527b7081ac206a728270f98
iud-wtag <imtiaj.uddin@welldev.io>
2023-05-30 17:56:24 +0600

    update todo propTypes

--- src/components/Todos/Todo/todo.jsx
+++ src/components/Todos/Todo/todo.jsx
@@ -12,9 +12,16 @@ const Todo = ({ todo }) => {
 
 Todo.propTypes = {
   todo: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     task: PropTypes.string.isRequired,
     date: PropTypes.instanceOf(Date).isRequired,
+    isTaskComplete: PropTypes.bool.isRequired,
+    completeTime: PropTypes.number,
   }),
+};
+
+Todo.defaultProps = {
+  completeTime: null,
 };
 
 export default Todo;
